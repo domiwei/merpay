8c1314902844d80e67a2252d74415040bcb752dc
kewei <kewei.train@gmail.com>
2019-08-11 03:00:22 +0800

    concurrently check conn state

--- mydb.go
+++ mydb.go
@@ -8,6 +8,10 @@ import (
 	"time"
 )
 
+var (
+	ErrDisconnected = fmt.Errorf("DB was disconnected")
+)
+
 type RWSplitDB struct {
 	master       *instance
 	readreplicas []*instance
@@ -15,41 +19,34 @@ type RWSplitDB struct {
 }
 
 func NewDB(master *sql.DB, readreplicas ...*sql.DB) (DB, error) {
+	// Wrap master and replicas by using instance struct
 	masterIns := NewDBInstance(master)
 	if state := masterIns.CheckConnection(); state != DBStateConnected {
 		return nil, fmt.Errorf("Cannot connect to master db")
 	}
-
-	// Check state of replicas concurrently
-	var wg sync.WaitGroup
 	replicaInses := []*instance{}
 	for i := range readreplicas {
 		replicaIns := NewDBInstance(readreplicas[i])
 		replicaInses = append(replicaInses, replicaIns)
-		wg.Add(1)
-		go func(replicaIns *instance) {
-			defer wg.Done()
-			replicaIns.CheckConnection()
-		}(replicaIns)
 	}
-	wg.Wait()
+	db := &RWSplitDB{
+		master:       masterIns,
+		readreplicas: replicaInses,
+	}
 
-	// Reject to init if all replicas are disconnected
-	someoneAlive := false
-	for _, r := range replicaInses {
-		if r.IsAlive() {
-			someoneAlive = true
-			break
+	// Check connection state for each replica
+	disConnCount := 0
+	for err := range db.concurrentlyDo(checkConn) {
+		if err != nil {
+			disConnCount++
 		}
 	}
-	if !someoneAlive {
+	// Reject if all replicas are disconnected
+	if disConnCount == len(replicaInses) {
 		return nil, fmt.Errorf("Cannot connect to any replica")
 	}
 
-	return &RWSplitDB{
-		master:       masterIns,
-		readreplicas: replicaInses,
-	}, nil
+	return db, nil
 }
 
 func (db *RWSplitDB) readReplicaRoundRobin() *instance {
@@ -59,6 +56,7 @@ func (db *RWSplitDB) readReplicaRoundRobin() *instance {
 
 func (db *RWSplitDB) Ping() error {
 	if err := db.master.Ping(); err != nil {
+		fmt.Errorf(err.Error())
 		return err
 	}
 
@@ -78,6 +76,7 @@ func (db *RWSplitDB) Ping() error {
 
 func (db *RWSplitDB) PingContext(ctx context.Context) error {
 	if err := db.master.PingContext(ctx); err != nil {
+		fmt.Errorf(err.Error())
 		return err
 	}
 
@@ -164,6 +163,8 @@ func (db *RWSplitDB) SetMaxOpenConns(n int) {
 	}
 }
 
+// concurrentlyDo invoke f for each replica concurrently. It returns a channel
+// to let caller receive the result of pre-defined function.
 func (db *RWSplitDB) concurrentlyDo(f func(dbIns *instance) error) <-chan error {
 	resultChan := make(chan error)
 	go func() {
@@ -172,6 +173,7 @@ func (db *RWSplitDB) concurrentlyDo(f func(dbIns *instance) error) <-chan error 
 			wg.Add(1)
 			go func(replica *instance) {
 				defer wg.Done()
+				// Pass result to channel
 				resultChan <- f(replica)
 			}(replica)
 		}
@@ -179,4 +181,13 @@ func (db *RWSplitDB) concurrentlyDo(f func(dbIns *instance) error) <-chan error 
 		close(resultChan)
 	}()
 	return resultChan
+}
+
+// checkConn is a helper func to check connection state. This function is passed as
+// an argument of concurrentlyDo.
+func checkConn(dbIns *instance) error {
+	if dbIns.CheckConnection() != DBStateConnected {
+		return ErrDisconnected
+	}
+	return nil
 }
