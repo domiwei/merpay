916c5f3033c185791fa238beac844090e149c2aa
Kewei Chen <kewei@17.media>
2019-08-12 12:18:31 +0800

    Skip to ping when timediff < 5

--- dbinstance.go
+++ dbinstance.go
@@ -3,39 +3,59 @@ package mydb
 import (
 	"database/sql"
 	"sync/atomic"
+	"time"
 )
 
 type instance struct {
 	*sql.DB
-	state DBState
+	state         DBState
+	lastCheckTime int64
+	pingLock      uint32
 }
 
 func NewDBInstance(db *sql.DB) *instance {
 	return &instance{
-		DB: db,
+		DB:    db,
 		state: DBStateDisconnected,
 	}
 }
 
 // IsAlive simply returns true if cached state is DBStateConnected
 func (i *instance) IsAlive() bool {
-	return i.state == DBStateConnected
-}
-
-// UpdateState atomically updates db state
-func (i *instance) UpdateState(oldState, newState DBState) (success bool) {
-	return atomic.CompareAndSwapInt32((*int32)(&i.state), int32(oldState), int32(newState))
+	return i.safeGetState() == DBStateConnected
 }
 
 // CheckConnection checks connection state and update it if needed
 func (i *instance) CheckConnection() DBState {
-	oldState := i.state
+	t := atomic.LoadInt64(&i.lastCheckTime)
+	if time.Now().Unix()-t < 5 {
+		// Skip to check if last check time within 5 sec
+		return i.safeGetState()
+	}
+
+	// Try lock so that only one thread is allowed to ping DB at a time.
+	if !atomic.CompareAndSwapUint32(&i.pingLock, 0, 1) {
+		return i.safeGetState()
+	}
+	defer atomic.StoreUint32(&i.pingLock, 0)
+	oldState := i.safeGetState()
 	newState := DBStateConnected
 	if err := i.DB.Ping(); err != nil {
 		newState = DBStateDisconnected
 	}
 	if oldState != newState {
-		i.UpdateState(oldState, newState)
+		i.updateState(oldState, newState)
 	}
+	// Update check time
+	i.lastCheckTime = time.Now().Unix()
 	return newState
+}
+
+// updateState atomically updates db state
+func (i *instance) updateState(oldState, newState DBState) (success bool) {
+	return atomic.CompareAndSwapInt32((*int32)(&i.state), int32(oldState), int32(newState))
+}
+
+func (i *instance) safeGetState() DBState {
+	return DBState(atomic.LoadInt32((*int32)(&i.state)))
 }
